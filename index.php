5626434b22eff423073c7a4239a0b49d65bfcee2
sandy ellen <sandy_e_silva@estudante.sc.senai.br>
2019-09-30 19:17:37 -0300

    Iniciando a aula03
    README atualizado

--- index.php
+++ index.php
@@ -1,141 +1,114 @@
 <!doctype html>
-<html lang="en">
-<!--
-     Para acessar o projeto, habilite o Apache    http://localhost/login/
+<html lang="pt-br">
+<!--    
+    Para acessar o projeto, habilite o Apache no XAMPP
+    Abra a URL http://localhost/login/ no navegador
 -->
+#login
+Sistema de Login em PHP e JavaScript
+
+# aula03 - 30/09/2019
+- Formulário de cadastro de novos usuários
+- Validação dos campos cm jQuery validate
+- ~Ocultar é uma biblioteca de funções javascript.
+
+# aula02 - 24/09/2019
+ Layout Bootstrap para os formulários
+- Formulário de login 
+
 <head>
     <!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-<title>Sistema de Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Sistema de Login</title>
 </head>
 
 <body class="bg-dark">
-    <!-- Fundo escuro-->
+    <!-- Fundo Escuro -->
     <main class="container mt-4">
         <section class="row">
             <div class="col-lg-4 offset-lg-4" id="alerta">
-                <div class="alert alert-sucess text-center">
+                <div class="alert alert-success text-center">
                     <strong class="resultado">
-                        Alo Ha Tchurmaaaaaaaaa
+                        Alo Ha Tchurmaáaaaaaaaaaaaaaaa!
                     </strong>
                 </div>
             </div>
         </section>
-        <!--Formulário de Login-->
+        <!-- Formulário de Login -->
         <section class="row">
             <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
-            <h2 class="text-center mt-2">
-                Entrar no Sistema
-            </h2>
-            <form action="#" method="post" class="p-2"
-            id="formLogin">
-                <div class="form-group">
-                   <input type="text" name="" id="nomeUsuario" id="nomeUsuario"
-                   placeholder="Nome de Usuário" class="form-control">
-                 </div>
+                <h2 class="text-center mt-2">
+                    Entrar no Sistema
+                </h2>
+                <form action="#" method="post" class="p-2" id="formLogin">
 
-                <div class="form-group">
-                    <input type="password" name="senheUsuario"
-                     id="senhaUsuario" placeholder="Senha"
-                     class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <div class="custom-control custom-checkbox">
-
-                        <input type="checkbox" name="lembrar" 
-                        id="lembrar" class="custom-control-input">
-
-                        <label for="lembrar" class="custom-control-label">
-                            Lembrar de mim
-                        </label>
-
-                        <a href="#" class="float-right" id="btnEsqueci">
-                            Esquecir a senha!
-
-                        </a>
+                    <div class="form-group">
+                        <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome de Usuário" class="form-control">
                     </div>
-                </div>
-
-                <div class="form-group">
-                    <input type="submit" value="::Entrar::"
-                    name="btnEntrar" id="btnEntrar"
-                    class="btn btn-primary btn-block">
-
-                </div>
-
-                <div class="form-group">
-                    <p class="text-center">
-                        Novo usuário? <a href="#" id="btnRegistrar">
-                        Registre-se aqui!
-                    </a>
-
-                    </p>
-
-                </div>
-
+                    <div class="form-group">
+                        <input type="password" name="senhaUsuario" id="senhaUsuario" placeholder="Senha" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" name="lembrar" id="lembrar" class="custom-control-input">
+                            <label for="lembrar" class="custom-control-label">
+                                Lembrar de mim.
+                            </label>
+                            <a href="#" class="float-right" id="btnEsqueci">
+                                Esqueci a senha!
+                            </a>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="::Entrar::" name="btnEntrar" id="btnEntrar" class="btn btn-primary btn-block">
+                    </div>
+                    <div class="form-group">
+                        <p class="text-center">
+                            Novo usuário? <a href="#" id="btnRegistrar">
+                                Registre-se aqui!
+                            </a>
+                        </p>
+                    </div>
                 </form>
             </div>
-        </section> 
-        <!--Final da Seção de Login-->
-            
-        <!--Foumulário de Recuperação de Senha-->
+        </section>
+        <!-- Final da Seção de Login-->
+
+        <!-- Formulário de Recuperação de Senha -->
         <section class="row mt-5">
-            <div class="col-lg-4 offset-lg-4 bg-light rounded"
-            id="caixaLongin">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
                 <h2 class="text-center mt-2">
                     Gerar Nova Senha
                 </h2>
-                <form action="#" method="post" id="formSenha"
-                class="p-2">
+                <form action="#" method="post" id="formSenha" class="p-2">
                     <div class="form-group">
                         <small class="text-muted">
-                            Para gerar uma nova senha, digite o 
+                            Para gerar uma nova senha, digite o
                             seu e-mail. Clique no link gerado.
-                         </small>
-
+                        </small>
                     </div>
-
                     <div class="form-group">
-                        <input type="email" name="emailGerarSenha"
-                         id="emailGerarSenha" class="form-control"
-                         placeholder="E-mail de recuperação de senha">
+                        <input type="email" name="emailGerarSenha" id="emailGerarSenha" class="form-control" placeholder="E-mail de recuperação de senha">
                     </div>
-
                     <div class="form-group">
-                        <input type="submit" value="::Gerar::"
-                        name="btnGerar" id="btnGerar"
-                        class="btn btn-primary btn-block">
-                     </div>
-
-                     <div class="form-group">
-                         <p class="text-center">
-                             Já registrado? 
-                             <a href="#" id="btnJaRegistrado">
+                        <input type="submit" value="::Gerar::" name="btnGerar" id="btnGerar" class="btn btn-primary btn-block">
+                    </div>
+                    <div class="form-group">
+                        <p class="text-center">
+                            Já registrado?
+                            <a href="#" id="btnJaRegistrado">
                                 Entrar por aqui.
-                             </a>
-                         </p>
-
-                     </div>
-
-                    
-
-
+                            </a>
+                        </p>
+                    </div>
                 </form>
-
-
             </div>
         </section>
-        <!--Fim da seção da Recuperação de Senha-->
-    
-
+        <!-- Fim da Seção de Recuperação de Senha -->
     </main>
-
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
