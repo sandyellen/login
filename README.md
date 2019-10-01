06f681a2179321e29b88e9b926d97d29115a9ccb
sandy ellen <sandy_e_silva@estudante.sc.senai.br>
2019-09-30 21:18:19 -0300

    Formulário de cadastro de novo usuario
    close #3

--- README.md
+++ README.md
@@ -2,47 +2,55 @@
 Sistema de Login em PHP e Javascript
 
 # aula03 - 30/09/2019
+ - Formulário de Cadastro de novos usuários
+ - Validação dos campos com jQuery validate
+ - ~Ocultar e Mostrar os formulários com jQuery~
+ 
+ jQuery é uma biblioteca de funções javascript.
+
+# aula02 - 24/09/2019 
 Layout Bootstrap para os formulários
 - Formulário de Login
-- Formulário de Recuperação de senha
-- Formulário de Cadastro de novos usuários
+- Formulário de recuperação de senha
+- ~Formulário de Cadastro de novos usuários~
 
-# aula - 23/09/2019
-🍀Começou a primavera🌺🌻🌹
-Aula inicial, configuração do projeto no Github
+# aula01 - 23/09/2019 :godmode:
+🌺🍀 Começou a Primavera 🌻🌷
+Aula inicial, configuração do projeto no Github.
 E criação do passo a passo.
+
 ---
 # Passo a passo em todo início de aula
 
 ## Habilitar o Proxy
-  Tecla Windows - Abrir a janela Prompt de Comando
-  Copie e cole a linha abaixo - selecione e pressione as teclas Ctrl C
+  - Tecla Windows - Abrir a janela Prompt de Comando
+  - Copie e cole a linha abaixo - selecione e pressione as teclas Ctrl C
 
-  git config --global http.proxy http://10.1.21.254:3128
-
-  Cole no 
-  Pressione Enter
-
-## Clonar o repositório **login**na pasta **c:\xampp\htdocs**
-  -Tecla Windows - Abrir o Github Desktop
-  -Clique em *Sing in to Github.com*
-  -Digite seu Login e Senha do Github
-  -Caso necessário, após o login, digite seu nome e-mail e clique em Continue*
-  -Selecione o repositório **login** e logo abaixo clique no botão azul *Clone...*
-  -Clique no botão *Choose* e encontre no disco local C: a pasta xampp e dentro dela htdocs
-  -Selecione a pasta e confira se o resultado foi *c:\xampp\htdocs\login*
-  -Clique no botão azul *Clone*
+    git config --global http.proxy http://10.1.21.254:3128
   
-## Habilitar o servidor web **Apache** e o servidor **MySQL** 
--Tecla Windows - Abrir o XAMPP Control Panel
--Clique em Start para o Apache
--Clique em Start para o MySQL
--Verifique se apareceu a porta 80 Apache e 3306 MySQL
--Verifique se ficou verde o Apache e MySQL
+  - Cole no Prompt de Comando e
+  - Pressione Enter
+
+## Clonar o repositório **login** na pasta **c:\xampp\htdocs**
+  - Tecla Windows - Abrir o Github Desktop
+  - Clique em *Sign in to Github.com*
+  - Digite seu Login e Senha do Github
+  - Caso necessário, após o login, digite seu nome e e-mail e clique em *Continue*
+  - Selecione o repositório **login** e logo abaixo clique no botão azul *Clone...*
+  - Clique no botão *Choose* e encontre no disco local C: a pasta xampp e dentro dela htdocs
+  - Selecione a pasta e confira se o *local path* foi *c:\xampp\htdocs\login*
+  - Clique no botão azul *Clone*
+
+## Habilitar o servidor web **Apache** e o servidor **MySQL**
+  - Tecla Windows - Abrir o XAMPP Control Panel
+  - Clique em Start para o Apache
+  - Clique em Start para o MySQL
+  - Verifique se apareceu a porta 80 Apache e 3306 MySQL
+  - Verifique se ficou verde o Apache e MySQL
 
 ## Testar se está funcionando
-  -Abra o navegador Firefox Azul e digite http://localhost/login
-  
+  - Abra o navegador Firefox Azul e digite http://localhost/login
+
 ## Editar utilizando o VS Code
-  -Tecla Windows - Abrir o Visual Studio Code (é o azul)
-  -*Abrir pasta* no VS Code escolha c:\xampp\htdocs\login
+  - Tecla Windows - Abrir o Visual Studio Code (é o azul)
+  - *Abrir Pasta* no VS Code escolha c:\xampp\htdocs\login
--- index.php
+++ index.php
@@ -4,18 +4,6 @@
     Para acessar o projeto, habilite o Apache no XAMPP
     Abra a URL http://localhost/login/ no navegador
 -->
-#login
-Sistema de Login em PHP e JavaScript
-
-# aula03 - 30/09/2019
-- Formulário de cadastro de novos usuários
-- Validação dos campos cm jQuery validate
-- ~Ocultar é uma biblioteca de funções javascript.
-
-# aula02 - 24/09/2019
- Layout Bootstrap para os formulários
-- Formulário de login 
-
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -23,6 +11,11 @@ Sistema de Login em PHP e JavaScript
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Sistema de Login</title>
+    <style>
+        #alerta, #caixaSenha, #caixaRegistro{
+            display: none;
+        }
+        </style>
 </head>
 
 <body class="bg-dark">
@@ -67,7 +60,7 @@ Sistema de Login em PHP e JavaScript
                     </div>
                     <div class="form-group">
                         <p class="text-center">
-                            Novo usuário? <a href="#" id="btnRegistrar">
+                            Novo usuário? <a href="#" id="btnRegistrarNOVO">
                                 Registre-se aqui!
                             </a>
                         </p>
@@ -79,7 +72,7 @@ Sistema de Login em PHP e JavaScript
 
         <!-- Formulário de Recuperação de Senha -->
         <section class="row mt-5">
-            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaSenha">
                 <h2 class="text-center mt-2">
                     Gerar Nova Senha
                 </h2>
@@ -108,12 +101,71 @@ Sistema de Login em PHP e JavaScript
             </div>
         </section>
         <!-- Fim da Seção de Recuperação de Senha -->
+
+        <!--Início do formulário de cadastro de novos usuários-->
+
+        <section class="row mt-5">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaRegistro">
+                <h2 class="text-center mt-2">Registre-se aqui</h2>
+                <form action="#" method="post" class="p-2" id="formRegistro">
+                    <div class=form-group>
+                        <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome completo">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" name="nomeUsuario" id="nomeUsuário" class="form-control" placeholder="Nome de usuário">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" name="emailUsuario" id="emailUsuario" class="form-control" placeholder="E-mail">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="password" name="senhaUsuario" id="senhaUsuario" class="form-control" placeholder="Senha">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="password" name="senhaUsuarioConfirmar" class="form-control" placeholder="Confirmar senha">
+                    </div>
+
+                    <div class="from-group">
+                        <div class="custon-control custom-checkbox">
+                            <input type="checkbox" name="concordar" id="concordar" class="custom-control-input">
+                            <label for="concordar" class="custom-control-label">
+                                Eu concordo com <a href="#"> os termos e condições.
+                            </label>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" value="::Registrar::" name="btnRegistrar" id="btnRegistrar" class="btn btn-primary btn-block">
+
+                        <div class="form-group">
+                            <p class="text-center">
+                                Já registrado?
+                                <a href="#" id="btnJaRegistrado2">
+                                    Entrar por aqui.
+                                </a>
+                            </p>
+                        </div>
+
+
+                </form>
+
+
+        </section>
+
+        <!--Final do formulário do cadastro de novos usuários-->
+
     </main>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script>
+        //Código jQuery para mostrar e ocultar os formulários
+    </script>
 </body>
 
 </html>
